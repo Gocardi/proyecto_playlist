8e50a2b7eea5914ea9b4667dfb00919fa8bddef4
L0rD1ego <diegoalejandrocarbajal@gmail.com>
2024-07-10 10:44:51 -0500

    Se modifico Cancion.java

---
+++ src/main/java/com/proyecto/modelo/Cancion.java
@@ -0,0 +1,26 @@
+package com.proyecto.modelo;
+
+public class Cancion {
+    private String artistName;
+    private String trackName;
+    private String trackId;
+    private int popularity;
+    private int year;
+    private String genre;
+    private double danceability;
+    private double energy;
+    private int key;
+    private double loudness;
+    private int mode;
+    private double speechiness;
+    private double acousticness;
+    private double instrumentalness;
+    private double liveness;
+    private double valence;
+    private double tempo;
+    private int durationMs;
+    private int timeSignature;
+
+    // Constructor, getters, and setters
+    // ...
+}
